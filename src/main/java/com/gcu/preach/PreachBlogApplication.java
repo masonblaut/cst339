81a5bbbfa5c45e551504f5e2b958d661f4d26667
elale1984 <elale@mygcu.edu>
2022-07-08 21:25:00 -0500

    init

---
+++ src/main/java/com/gcu/preach/PreachBlogApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.preach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreachBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreachBlogApplication.class, args);
+	}
+
+}
